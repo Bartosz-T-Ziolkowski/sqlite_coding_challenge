6a12602544fb2e427c682aae070815ed32179781
Bartosz <bziolkowski@usf.edu>
2025-11-16 15:17:20 -0500

    Finished Task 1 — Top 5 Customers by Total Spend

---
+++ challenge.sql
@@ -0,0 +1,7 @@
+SELECT c.first_name || ' ' || c.last_name AS customer_name, SUM(oi.quantity * oi.unit_price) AS total_spend
+FROM customers AS c
+JOIN orders AS o ON o.customer_id = c.id
+JOIN order_items AS oi ON oi.order_id = o.id
+GROUP BY c.id
+ORDER BY total_spend DESC
+LIMIT 5;
