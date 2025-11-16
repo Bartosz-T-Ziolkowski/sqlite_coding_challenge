bdb4c1a20e3df7af160e608873f8a2fa701ebf6d
Bartosz <bziolkowski@usf.edu>
2025-11-16 15:39:39 -0500

    Finished Task 3 — Employees Earning Above Their Department Average

--- challenge.sql
+++ challenge.sql
@@ -12,3 +12,9 @@ JOIN order_items AS oi ON oi.product_id = p.id
 JOIN orders AS o ON o.id = oi.order_id
 GROUP BY p.category
 ORDER BY revenue DESC;
+
+SELECT e.first_name, e.last_name, d.name AS department_name, e.salary, (SELECT AVG(e2.salary) FROM employees AS e2 WHERE e2.department_id = e.department_id) AS avg_department_salary
+FROM employees AS e
+JOIN departments AS d ON d.id = e.department_id
+WHERE e.salary > (SELECT AVG(e3.salary) FROM employees AS e3 WHERE e3.department_id = e.department_id)
+ORDER BY d.name, e.salary DESC;
