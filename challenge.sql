03090f28f2c4446c7d73e69ed64e42f269bb2755
Bartosz <bziolkowski@usf.edu>
2025-11-16 15:20:18 -0500

    Finished Task 2 — Total Revenue by Product Category

--- challenge.sql
+++ challenge.sql
@@ -5,3 +5,10 @@ JOIN order_items AS oi ON oi.order_id = o.id
 GROUP BY c.id
 ORDER BY total_spend DESC
 LIMIT 5;
+
+SELECT p.category, SUM(oi.quantity * oi.unit_price) AS revenue
+FROM products AS p
+JOIN order_items AS oi ON oi.product_id = p.id
+JOIN orders AS o ON o.id = oi.order_id
+GROUP BY p.category
+ORDER BY revenue DESC;
